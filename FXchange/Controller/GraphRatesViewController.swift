fae2509a345df2eca6fd336c71c7e9293cfe10d5
OLAJUWON <BADGUY@jays-macbook-pro.local>
2018-08-29 11:17:49 +0100

    Networking using alamofire For historical exchange rates

--- FXchange/Controller/GraphRatesViewController.swift
+++ FXchange/Controller/GraphRatesViewController.swift
@@ -7,19 +7,53 @@
 //
 
 import UIKit
+import Charts
+import Alamofire
+import SwiftyJSON
 
 class GraphRatesViewController: UIViewController {
 
+    
+    var baseUrl = "http://data.fixer.io/api/timeseries"
+    var key = "5d9c40384cba4c796a12323c104f35d6"
+    var symbol  = "USD"
+    var startdate = "2018-08-01"
+    var end_date = "2018-08-25"
+    var base = "EUR"
+    
+    
+    
+    @IBOutlet weak var LineGraph: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let params = ["access_key": key, "start_date": startdate, "end_date": end_date, "base": base, "symbols": symbol]
+        
+        getHistoryData(url: baseUrl, Parameters: params)
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    //Networking 
+    func getHistoryData(url: String ,Parameters: [String:String]){
+        
+        Alamofire.request(url, method: .get, parameters: Parameters).responseJSON { response in
+            if response.result.isSuccess{
+                print("Got data")
+                let dataJSON: JSON = JSON(response.result.value!)
+                print(dataJSON)
+            }else {
+                print("Error \(String(describing: response.result.error ))")
+            }
+        }
+        
+    }
+    
     
 
 }
